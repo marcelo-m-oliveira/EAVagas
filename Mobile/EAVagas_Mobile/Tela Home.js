c9ece74ce8729cfb889061f3e092d68307574a7b
Marcelo Moreira de Oliveira <maxcelo.lima1000@gmail.com>
2018-06-04 19:51:51 -0300

    Atualizações no mobile

---
+++ Mobile/EAVagas_Mobile/Tela Home.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {Alert, Platform, StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
+
+
+
+export default class App extends Component {
+  
+  render(){
+    return(
+      <View style={styles.fundo}>
+        <View>
+        <Image style = {styles.imagem1} source = {require("./imagens/logo.png")}/>
+        <Text style={styles.texto2}>EA Vagas</Text>
+        <Text style={styles.texto}>Você está em: Paris</Text>
+        <Text style={styles.texto3}> Aqui você escolhe onde quer estacionar 
+        o seu veículo lindo.</Text>
+        
+        
+        <Image style = {styles.imagem2} source = {require("./imagens/paris.png")}/>
+        
+        </View>
+      </View>
+      
+      )
+  }
+}
+
+const styles = StyleSheet.create({
+  
+  texto:{
+    marginTop: 35,
+    color: '#000000',
+    fontSize: 25,
+    alignSelf: 'center',
+    marginBottom: 50,
+    fontWeight: 'bold'
+  },
+  imagem1:{
+    height: 50,
+    resizeMode: 'contain',
+    marginLeft: -80,
+    marginTop: 40
+  },
+  texto2:{
+    marginLeft: 70,
+    marginTop: -40,
+    fontSize: 15
+  },
+  texto3:{
+    color: '#585858',
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 15,
+    marginTop: -30,
+    marginBottom: 30
+  },
+  imagem2:{
+    resizeMode: 'contain',
+    height: 500,
+    marginLeft: -25
+  },
+  imagem3:{
+    resizeMode: 'contain',
+    height: 50,
+    alignSelf: 'center'
+  }
+});
---
+++ Mobile/EAVagas_Mobile/Tela Preferencias.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import {Alert, Platform, StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ImageBackground} from 'react-native';
+
+
+
+export default class App extends Component {
+  
+  render(){
+    return(
+      <View style={styles.fundo}>
+        <View>
+        <TouchableOpacity style={styles.botao1}>
+          <Text style={styles.textoBotao1}> Voltar </Text>
+        </TouchableOpacity>
+        
+        <Text style={styles.texto}>Preferências</Text>
+        
+        <TouchableOpacity style={styles.botao2}>
+          <Text style={styles.textoBotao2}> Conta </Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.botao3}>
+          <Text style={styles.textoBotao3}> Notificações </Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.botao4}>
+          <Text style={styles.textoBotao4}> Ajuda </Text>
+        </TouchableOpacity>
+        
+        </View>
+      </View>
+      
+      )
+  }
+}
+
+const styles = StyleSheet.create({
+  
+  texto:{
+    marginTop: 20,
+    color: '#585858',
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 50,
+    fontWeight: 'bold'
+  },
+  botao1:{
+    marginTop: 60,
+    backgroundColor: "#DA0505",
+    width: 70,
+    marginLeft: 15,
+    padding: 10,
+    borderRadius: 7
+  },
+  textoBotao1:{
+    color: '#FFFFFF',
+    fontSize: 13,
+    alignSelf: 'center'
+  },
+  botao2:{
+    marginLeft: 20,
+    padding: 10
+  },
+  textoBotao2:{
+    fontSize: 25
+  },
+  botao3:{
+    marginLeft: 20,
+    padding: 10
+  },
+  textoBotao3:{
+    fontSize: 25
+  },
+  botao4:{
+    marginLeft: 20,
+    padding: 10
+  },
+  textoBotao4:{
+    fontSize: 25
+  }
+ 
+  
+  
+});
