b7f78497fbd415523cb16c3859fc7f832f488cfc
Unknown <lucassantana.profissional@gmail.com>
2018-06-14 19:36:58 -0300

    Telas de cartão linkadas

--- Desktop/src/controller/CadastraCartaoController.java
+++ Desktop/src/controller/CadastraCartaoController.java
@@ -1,35 +1,29 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CadastraCartaoController {
 	
 	public static final String tituloCadastroCartao = "Cadastro de Cartão de Crédito";
 
-    private Label LabelInsiraNumero;
     private TextField TextFieldNumero;
     private TextField TextFieldData;
     private TextField TextFieldCodigo;
-    private Button ButtonConfirmar;
-    private CheckBox CheckBoxTermos;
-    
-    public void cadastrar(ActionEvent event) {
-    	if(CartaoController.cadastrarCartao(TextFieldNumero.getText(), TextFieldCodigo.getText(), 
+    public void cadastrarCartao(ActionEvent event) {
+    	if(CartaoController.cadastraCartao(TextFieldNumero.getText(), TextFieldCodigo.getText(), 
     			TextFieldData.getText())) {
-    		TelaController.exibirJanela(AlertType.CONFIRMATION, CadastraCartaoController.tituloCadastroCartao, 
+    		TelaController.exibirJanela(AlertType.INFORMATION, CadastraCartaoController.tituloCadastroCartao, 
     				"Cadastro bem-sucedido", "Cadastro de Cartão de Crédito efetuado com sucesso!");
+    		limpaCampos();
     		TelaController.carregarTela(event, "/application/Home.fxml", HomeController.telaHome);
     	}else {
     		TelaController.exibirJanela(AlertType.ERROR, CadastraCartaoController.tituloCadastroCartao, 
     				"Falha no cadastro", "Falha ao cadastrar cartão de crédito. Verifique os dados preenchidos e tente novamente.");
     	}
     }
+    
     
     public void telaAnterior(ActionEvent event) {
     	TelaController.carregarTela(event, "/application/FacaCartao.fxml", FacaCartaoController.tituloFacaCartao);
