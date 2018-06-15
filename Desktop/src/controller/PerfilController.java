c6753fa4acd4f51164cc8c7fc222f9581279d21b
Unknown <lucassantana.profissional@gmail.com>
2018-06-15 00:11:08 -0300

    seu cu

--- Desktop/src/controller/PerfilController.java
+++ Desktop/src/controller/PerfilController.java
@@ -10,28 +10,14 @@ public class PerfilController {
 	
 	public static final String telaPerfil = "Perfil";
 
-    @FXML
+    
     private Label LabelPerfil;
-
-    @FXML
     private ImageView ImageViewFoto;
-
-    @FXML
     private Label LabelMembro;
-
-    @FXML
     private Label LabelNome;
-
-    @FXML
     private Label LabelVeiculo;
-
-    @FXML
     private Label LabelEstacionamento;
-
-    @FXML
     private Button ButtonSair;
-
-    @FXML
     private Button ButtonVoltar;
     
     public void voltarHome(ActionEvent event) {
